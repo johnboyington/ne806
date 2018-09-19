29cc0b1168a53669642018c57a8ac098106dee7e
John Boyington <mjcb@ksu.edu>
2018-09-19 14:58:40 -0500

    ran mcnp longer; modifications to hw1

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -2,9 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def f(w, psi):
-    return (1 / (2*np.pi)) * (1 + w)
+def f1(w, psi):
+    return (1 / (2)) * (1 + w)
 
+
+def f2(w, psi):
+    """Technically not correct, but produces proper pdf."""
+    return (1 / (2*np.pi)) * (1 + w)
 
 
 fig = plt.figure(0)
@@ -14,7 +18,7 @@ ax.set_ylabel('$f(\psi)$')
 
 w = np.zeros(100)
 psi = np.linspace(0, 2*np.pi, 100)
-ax.plot(psi, f(w, psi), color='k')
+ax.plot(psi, f2(w, psi), color='k')
 
 
 fig = plt.figure(1)
@@ -23,4 +27,4 @@ ax.set_xlabel('$\omega_c$')
 ax.set_ylabel('$f(\omega_c)$')
 
 w = np.linspace(-1, 1, 100)
-ax.plot(w, f(w, psi), color='k')
+ax.plot(w, f1(w, psi), color='k')
