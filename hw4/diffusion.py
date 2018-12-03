f9e6d9d60aee983e053d2e2322a859ef0cc71103
John Boyington <mjcb@ksu.edu>
2018-12-03 11:56:11 -0600

    finished second part of hw; produced plots

--- hw4/diffusion.py
+++ hw4/diffusion.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 def diffusion(n, T, D, Sig_a, S_0, source, geometry='slab'):
     """A finite difference diffusion solver."""
 
+    # guarantee geometry in correct format
+    assert geometry in ['slab', 'cylinder', 'sphere'], "Geometry must either be 'slab', 'cylinder' or 'sphere'."
+
     # calculate L
     L = np.sqrt(D / Sig_a)
 
@@ -16,7 +19,6 @@ def diffusion(n, T, D, Sig_a, S_0, source, geometry='slab'):
     A = np.zeros((len(x), len(x)))
 
     # set up source vector
-    print(source)
     s = source(x, S_0) / D
 
     # loop through x's and update A matrix
@@ -24,25 +26,47 @@ def diffusion(n, T, D, Sig_a, S_0, source, geometry='slab'):
 
         # first boundary condition
         if i == 0:
-            A[i, i] = (1/4) - (D / (2 * delta))
-            A[i, i + 1] = (D / (2 * delta))
-            s[i] = 0
+            if geometry == 'slab':
+                A[i, i] = (1/4) - (D / (2 * delta))
+                A[i, i + 1] = (D / (2 * delta))
+                s[i] = 0
+
+            elif geometry in ['sphere', 'cylinder']:
+                A[i, i] = (-1) / delta
+                A[i, i + 1] = 1 / delta
+                s[i] = 0
 
         # second boundary condition
         elif i == len(x) - 1:
-            A[i, i] = (1/4) - (D / (2 * delta))
-            A[i, i - 1] = (D / (2 * delta))
-            s[i] = 0
+            if geometry == 'slab':
+                A[i, i] = (1/4) - (D / (2 * delta))
+                A[i, i - 1] = (D / (2 * delta))
+                s[i] = 0
+
+            elif geometry in ['sphere', 'cylinder']:
+                A[i, i - 1] = (1/4) - (D / (2 * delta))
+                A[i, i] = (D / (2 * delta))
+                s[i] = 0
 
         # for every other point
         else:
-            A[i, i - 1] = (-1) / delta**2
-            A[i, i] = (2 / delta**2) + (1 / L**2)
-            A[i, i + 1] = (-1) / delta**2
+            if geometry == 'slab':
+                A[i, i - 1] = (-1) / delta**2
+                A[i, i] = (2 / delta**2) + (1 / L**2)
+                A[i, i + 1] = (-1) / delta**2
+
+            elif geometry == 'sphere':
+                A[i, i - 1] = (-1) / delta**2
+                A[i, i] = (x[i+1]**2/(x[i]**2 * delta**2) + 1 / delta**2 + 1 / L**2)
+                A[i, i + 1] = (-x[i+1]**2) / (x[i]**2 * delta**2)
+
+            elif geometry == 'cylinder':
+                A[i, i - 1] = (-1) / delta**2
+                A[i, i] = (x[i+1]/(x[i] * delta**2) + 1 / delta**2 + 1 / L**2)
+                A[i, i + 1] = (-x[i+1]) / (x[i] * delta**2)
 
     # solve the system of equations
-    phi = np.linalg.solve(A, s)
-    return phi
+    return np.linalg.solve(A, s)
 
 
 def source_a(x, S_0):
@@ -52,10 +76,26 @@ def source_a(x, S_0):
 
 def source_b(x, S_0):
     """The distributed source for parts b and c."""
-    return S_0
+    return S_0 * np.ones(len(x))
 
+
+def plot_it(phi, label, savename):
+    """Utility for plotting."""
+    x = np.linspace(0, 45, 100)
+    plt.figure(0)
+    plt.xlabel('$\hat{r}$')
+    plt.ylabel('$\Phi$')
+    plt.plot(x, phi, 'k', label=label)
+    plt.legend()
+    plt.savefig(savename + '.png', dpi=300)
+    plt.clf()
 
 if __name__ == '__main__':
-    phi = diffusion(100, 45, 0.600, 0.005, 10**8, source_a)
-    plt.figure(0)
-    plt.plot(phi)
+    phi_slab = diffusion(100, 45, 0.600, 0.005, 10**8, source_a, geometry='slab')
+    phi_cyl = diffusion(100, 45, 0.600, 0.005, 10**8, source_b, geometry='cylinder')
+    phi_sph = diffusion(100, 45, 0.600, 0.005, 10**8, source_b, geometry='sphere')
+
+    # plot the slab
+    plot_it(phi_slab, 'Slab', 'slab')
+    plot_it(phi_cyl, 'Cylinder', 'cyl')
+    plot_it(phi_sph, 'Sphere', 'sph')
