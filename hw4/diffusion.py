5758edbe8736577bdcdb8043abfa7d416b72ec2a
John Boyington <mjcb@ksu.edu>
2018-12-03 14:44:11 -0600

    updated pictures

--- hw4/diffusion.py
+++ hw4/diffusion.py
@@ -79,24 +79,35 @@ def source_b(x, S_0):
     return S_0 * np.ones(len(x))
 
 
-def plot_it(phi, label, savename):
+def plot_it(phi, n, label, savename):
     """Utility for plotting."""
-    x = np.linspace(0, 45, 100)
     plt.figure(0)
     plt.xlabel('$\hat{r}$')
     plt.ylabel('$\Phi$')
-    plt.plot(x, phi, 'k', label=label)
+    for i, p in enumerate(phi):
+        plt.plot(np.linspace(0, 45, n[i]), p, label=label[i], linewidth=0.5)
     plt.legend()
     plt.savefig(savename + '.png', dpi=300)
     plt.clf()
 
 
 if __name__ == '__main__':
-    phi_slab = diffusion(100, 45, 0.600, 0.005, 10**8, source_a, geometry='slab')
-    phi_cyl = diffusion(100, 45, 0.600, 0.005, 10**8, source_b, geometry='cylinder')
-    phi_sph = diffusion(100, 45, 0.600, 0.005, 10**8, source_b, geometry='sphere')
+    # slab geometry
+    phi_slab = [diffusion(10, 45, 0.600, 0.005, 10**8, source_a, geometry='slab'),
+                diffusion(100, 45, 0.600, 0.005, 10**8, source_a, geometry='slab'),
+                diffusion(1000, 45, 0.600, 0.005, 10**8, source_a, geometry='slab')]
+
+    # cylindrical geometry
+    phi_cyl = [diffusion(10, 45, 0.600, 0.005, 10**8, source_b, geometry='cylinder'),
+               diffusion(100, 45, 0.600, 0.005, 10**8, source_b, geometry='cylinder'),
+               diffusion(1000, 45, 0.600, 0.005, 10**8, source_b, geometry='cylinder')]
+
+    # spherical geometry
+    phi_sph = [diffusion(10, 45, 0.600, 0.005, 10**8, source_b, geometry='sphere'),
+               diffusion(100, 45, 0.600, 0.005, 10**8, source_b, geometry='sphere'),
+               diffusion(1000, 45, 0.600, 0.005, 10**8, source_b, geometry='sphere')]
 
     # plot the slab
-    plot_it(phi_slab, 'Slab', 'slab')
-    plot_it(phi_cyl, 'Cylinder', 'cyl')
-    plot_it(phi_sph, 'Sphere', 'sph')
+    plot_it(phi_slab, [10, 100, 1000], ['Slab n=10', 'Slab n=100', 'Slab n=1000'], 'slab')
+    plot_it(phi_cyl, [10, 100, 1000], ['Cylinder n=10', 'Cylinder n=100', 'Cylinder n=1000'], 'cyl')
+    plot_it(phi_sph, [10, 100, 1000], ['Sphere n=10', 'Sphere n=100', 'Sphere n=1000'], 'sph')
